2638e85d00358c824cb50adbf73d6da4876967d4
Dave <daven.waay18@gmail.com>
2026-05-13 16:41:14 +0800

    passing props

--- Scrim-s06ir1i/App.jsx
+++ Scrim-s06ir1i/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import avatar from "./images/user.png"
+import Star from "./Star"
 import starFilled from "./images/star-filled.png"
 import starEmpty from "./images/star-empty.png"
 
@@ -9,7 +10,7 @@ export default function App() {
         lastName: "Doe",
         phone: "+1 (212) 555-1212",
         email: "itsmyrealname@example.com",
-        isFavorite: false
+        isFavorite: true
     })
     
     let starIcon = contact.isFavorite ? starFilled : starEmpty
@@ -42,17 +43,7 @@ export default function App() {
                     alt="User profile picture of John Doe"
                 />
                 <div className="info">
-                    <button
-                        aria-pressed={contact.isFavorite}
-                        aria-label={contact.isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        className="favorite-button"
-                    >
-                        <img
-                            src={starIcon}
-                            alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
-                            className="favorite"
-                        />
-                    </button>
+                    <Star isFilled={contact.isFavorite}/>
                     <h2 className="name">
                         {contact.firstName} {contact.lastName}
                     </h2>
